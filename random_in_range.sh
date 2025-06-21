de0d907fb3f90ed55622af5ca37ef4be07eb30d2
Dinika Saxena <dinika@greyllama.cc>
2025-06-21 17:26:28 +0200

    Use shuf to get a random number

--- random_in_range.sh
+++ random_in_range.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -oeu pipefail
-set -o xtrace
 
 low=${1:-0}
 high=${2:-32767}
@@ -12,5 +11,5 @@ fi
 
 random_num=$RANDOM
 
-result=$(( $random_num % ($high - $low + 1) + $low  ))
+result=$(shuf --input-range=$low-$high --head-count=1)
 echo "Random number between $low and $high is: $result"
