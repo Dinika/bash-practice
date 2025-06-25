a3cde4c37566a7e6311a60b7246c45170cb096b1
Dinika Saxena <dinika@greyllama.cc>
2025-06-25 10:42:37 +0200

    Add steps for invoice generaton

---
+++ invoice.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+INVOICE_DIR="~/Documents/Work/Automattic/Invoices"
+INVOICE_TEMPLATE="$INVOICE_DIR/invoice-template.html"
+
+# Step 1: Make a copy of invoice_template by name "Invoice <month> <year> Dinika Saxena"
+
+# Step 2: Extract the invoice-number from COPY_INVOICE_TEMPLATE"
+
+# Step 3: Increment the number + check the year
+
+# Step 4: Replace the old invoice number with new one in both - INVOICE_TEMPLATE and COPY_INVOICE_TEMPLATE
+
+# Step 5: Replace the {{DATE}} in COPY_INVOICE_TEMPLATE to current date YYYY-MM-DD
+
+# Step 6: Convert COPY_INVOICE_TEMPLATE to pdf
+
+# Step 7: Delete the `COPY_INVOICE_TEMPLATE` html file
+
+# Step 8: Echo location of pdf
